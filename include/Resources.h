5b5a2d006401cb239af0c9e54db058fe8dacb203
Unknown <ftfnunes@hotmail.com>
2018-03-29 14:29:18 -0300

    Criacao de arquivo de resource

---
+++ include/Resources.h
@@ -0,0 +1,14 @@
+//
+// Created by ftfnunes on 29/03/18.
+//
+
+#ifndef T1_RESOURCEMANAGER_H
+#define T1_RESOURCEMANAGER_H
+
+
+class Resources {
+
+};
+
+
+#endif //T1_RESOURCEMANAGER_H
---
+++ src/Resources.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ftfnunes on 29/03/18.
+//
+
+#include "Resources.h"
